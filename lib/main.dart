65589652145a85a906597a87ac49f3e458d4ba43
KevinMendoza <2510662018@mail.utec.edu.sv>
2023-08-20 22:19:31 -0600

    Commit Inicial
    Pantalla de Bienvenida Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -9,10 +9,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(image: AssetImage('assets/logo.png'),width: 250,height: 200,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text('Gas',
+                  style: GoogleFonts.koulen(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 48,
+                    color: const Color(0xff3e4c6b)
+                  )
+                ),
+                Text('Finder',
+                    style: GoogleFonts.koulen(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 48,
+                        color:const Color(0xffd22833)
+                    )
+                )
+              ],)
+            ],
+          )
         ),
       ),
     );
